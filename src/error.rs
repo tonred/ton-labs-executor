13b404ffda3cce810ff0e3ffea082cb4b1282d5e
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-08-13 18:47:58 +0300

    Update dependencies. swap failure to anyhow
    Update dependencies. swap failure to anyhow

--- src/error.rs
+++ src/error.rs
@@ -15,18 +15,18 @@ use ton_types::types::ExceptionCode;
 use ton_block::ComputeSkipReason;
 use ton_vm::stack::StackItem;
 
-#[derive(Debug, failure::Fail, PartialEq)]
+#[derive(Debug, thiserror::Error, PartialEq)]
 pub enum ExecutorError {   
-    #[fail(display = "Invalid external message")]
+    #[error("Invalid external message")]
     InvalidExtMessage,
-    #[fail(display = "Transaction executor internal error: {}", 0)]
+    #[error("Transaction executor internal error: {}", .0)]
     TrExecutorError(String),
-    #[fail(display = "VM Exception, code: {}", 0)]
+    #[error("VM Exception, code: {}", 0)]
     TvmExceptionCode(ExceptionCode),
-    #[fail(display = "Contract did not accept message, exit code: {}", 0)]
+    #[error("Contract did not accept message, exit code: {}", 0)]
     NoAcceptError(i32, Option<StackItem>),
-    #[fail(display = "Cannot pay for importing this external message")]
+    #[error("Cannot pay for importing this external message")]
     NoFundsToImportMsg,
-    #[fail(display = "Compute phase skipped while processing exteranl inbound messagewith reason {:?}", 0)]
+    #[error("Compute phase skipped while processing exteranl inbound messagewith reason {:?}", 0)]
     ExtMsgComputeSkipped(ComputeSkipReason)
 }
