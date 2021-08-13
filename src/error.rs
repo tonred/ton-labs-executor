34a25d80d0a7fa8bb5ed4b3e71684198322ad2ea
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-08-13 18:56:10 +0300

    Fix errors

--- src/error.rs
+++ src/error.rs
@@ -19,14 +19,14 @@ use ton_vm::stack::StackItem;
 pub enum ExecutorError {   
     #[error("Invalid external message")]
     InvalidExtMessage,
-    #[error("Transaction executor internal error: {}", .0)]
+    #[error("Transaction executor internal error: {0}")]
     TrExecutorError(String),
-    #[error("VM Exception, code: {}", 0)]
+    #[error("VM Exception, code: {0}")]
     TvmExceptionCode(ExceptionCode),
-    #[error("Contract did not accept message, exit code: {}", 0)]
+    #[error("Contract did not accept message, exit code: {0}")]
     NoAcceptError(i32, Option<StackItem>),
     #[error("Cannot pay for importing this external message")]
     NoFundsToImportMsg,
-    #[error("Compute phase skipped while processing exteranl inbound messagewith reason {:?}", 0)]
+    #[error("Compute phase skipped while processing exteranl inbound messagewith reason {0:?}")]
     ExtMsgComputeSkipped(ComputeSkipReason)
 }
